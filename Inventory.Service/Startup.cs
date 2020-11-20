5767184ab1d91707ad73a772c5edfb28e8a162c6
Amit Kumar <amitverma1150@gmail.com>
2020-11-20 12:26:11 +0530

    namespace changed

--- Inventory.Service/Startup.cs
+++ Inventory.Service/Startup.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Inventory.Business;
-using Inventory.Core.Business;
-using Inventory.Core.Repositories;
+using Inventory.Core.Contracts.Business;
+using Inventory.Core.Contracts.Repository;
 using Inventory.Data;
-using Inventory.Data.Repositories;
+using Inventory.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Inventory.Service
 {
